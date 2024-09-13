e17231a0d389a05f135dedaa24e0a6a658992148
Shariar Hasan <101088151+Utchash007@users.noreply.github.com>
2024-09-13 23:58:03 +0600

    Time: 0 ms (100%), Space: 8.3 MB (46.03%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int reverse(int x) {
+          long  m=x;
+          m=abs(m);
+        long t=0;
+        long long rev =0;
+
+        while(m>0)
+        {
+            t=m%10;
+            m=m/10;
+            rev= (rev*10)+t;
+            if(rev>INT_MAX)
+            {return 0;
+            }
+        }
+        if(x>0)
+        return rev;
+        else 
+        return  -rev;
+    }
+};
