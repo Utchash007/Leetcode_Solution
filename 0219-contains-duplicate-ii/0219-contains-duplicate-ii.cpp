01d9e9a85f5829e9d168b4e65d9c3fec35607950
Shariar Hasan <101088151+Utchash007@users.noreply.github.com>
2024-09-14 10:38:39 +0600

    Time: 2588 ms (5.04%), Space: 62.9 MB (97.69%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -5,5 +5,12 @@ public:
         for(int j=0; j+i<nums.size(); j++)
         if(nums[j]==nums[i+j])return true;
         return false;
+        //beated memory not time
     }
 };
+auto boom = [](){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 0;
+}();
