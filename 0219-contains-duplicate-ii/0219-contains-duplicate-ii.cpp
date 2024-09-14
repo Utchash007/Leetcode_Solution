3f2f5d748357e0cca69e92042e779bcdf653efc1
Shariar Hasan <101088151+Utchash007@users.noreply.github.com>
2024-09-14 10:39:06 +0600

    Time: 277 ms (11.02%), Space: 104.7 MB (5%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        for(int i=1; i<=k;i++)
-        for(int j=0; j+i<nums.size(); j++)
-        if(nums[j]==nums[i+j])return true;
+        map<int,bool>ase; map<int,int>mp;
+        for(int i=0; i<nums.size(); i++){
+            if(ase[nums[i]]){
+                if(abs(mp[nums[i]]-i)<=k)return true;
+            }
+            ase[nums[i]]=1; mp[nums[i]]=i;
+        }
         return false;
-        //beated memory not time
+        
     }
 };
-auto boom = [](){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 0;
-}();
