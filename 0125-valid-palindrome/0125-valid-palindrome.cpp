133e7cce56c12adda93a6fe976892d9cddcfdf5b
Shariar Hasan <101088151+Utchash007@users.noreply.github.com>
2024-09-19 20:49:28 +0600

    Time: 10 ms (9.31%), Space: 9.6 MB (16.26%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -3,14 +3,20 @@ public:
     bool isPalindrome(string s) {
         //s.erase(remove(s.begin(), s.end(),' '));
         transform(s.begin(), s.end(), s.begin(), :: tolower);
-        cout<<s<<endl;
+        //cout<<s<<endl;
         string ans="";
         for(auto &u:s){
             if((u>='a' && u<='z' )|| (u>='0' && u<='9'))ans+=u;
         }
         string s1=ans;
         reverse(s1.begin(), s1.end());
-        cout<<ans<<" "<<s1<<"\n";
+        //cout<<ans<<" "<<s1<<"\n";
         return (ans==s1);
     }
+};
+
+auto boom = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0); 
 };
