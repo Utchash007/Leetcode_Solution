98fcbbb9a246ca3ba48847b225ff7fec12c1885a
Shariar Hasan <101088151+Utchash007@users.noreply.github.com>
2024-09-19 20:45:28 +0600

    Time: 8 ms (34.62%), Space: 9.7 MB (12.59%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        //s.erase(remove(s.begin(), s.end(),' '));
+        transform(s.begin(), s.end(), s.begin(), :: tolower);
+        cout<<s<<endl;
+        string ans="";
+        for(auto &u:s){
+            if((u>='a' && u<='z' )|| (u>='0' && u<='9'))ans+=u;
+        }
+        string s1=ans;
+        reverse(s1.begin(), s1.end());
+        cout<<ans<<" "<<s1<<"\n";
+        return (ans==s1);
+    }
+};
