9da3e50733a377d57261459b5e5aed56ab38993b
Shariar Hasan <101088151+Utchash007@users.noreply.github.com>
2024-09-20 21:05:27 +0600

    Time: 185 ms (73.03%), Space: 12.8 MB (33.3%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     int countPrimes(int n) {
         if(n==0 || n==1)return 0;
-        vector<bool>st(n+1,false);
+        bool st[n+1];
+        for(int i=0; i<=n; i++)st[i]=0;
         for(int i=2; i<=n; i++){
             if(!st[i]){for(int j=i+i; j<=n; j+=i)st[j]=1;}
         }
