471707f916cb0d8bd0fa446f29b2dd98af7b2417
Shariar Hasan <101088151+Utchash007@users.noreply.github.com>
2024-10-08 20:38:30 +0600

    Time: 6 ms (5.23%), Space: 8.2 MB (80.4%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,29 +1,17 @@
 class Solution {
 public:
     int reverse(int x) {
-          long  m=x;
-          m=abs(m);
-        long t=0;
-        long long rev =0;
+          long long int m=abs(x);
+          long long int ans=0;
 
-        while(m>0)
-        {
-            t=m%10;
-            m=m/10;
-            rev= (rev*10)+t;
-            if(rev>INT_MAX)
-            {return 0;
-            }
-        }
-        if(x>0)
-        return rev;
-        else 
-        return  -rev;
+          while(m>0){
+                int p=m%10;
+                ans=(ans*10)+p;
+                m/=10;
+          }
+          if(ans>INT_MAX)return 0;
+          if(x<0)return -ans;
+          return ans;
+          
     }
 };
-auto boom = [](){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 0;
-}();
