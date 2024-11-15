6352f443ee753b247bdf74ab56c0daf2bb434ae2
Shariar Hasan <101088151+Utchash007@users.noreply.github.com>
2024-11-15 19:41:30 +0600

    Time: 4 ms (7.81%), Space: 10.6 MB (5.17%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,20 +1,17 @@
 class Solution {
-    using ll=int;
+   using ll= long long int ;
 public:
-    bool isSubsequence(string s, string p) {
-        int n=s.size(),m=p.size();
-       s="*"+s;p="*"+p;
-       ll dp[n+1][m+1];memset(dp,0,sizeof(dp));
-       for(ll i=1; i<=n; i++){
-        for(ll j=1; j<=m; j++){
-            ll milse=(s[i]==p[j])?1+dp[i-1][j-1]:0;
-            ll not_milse=max(dp[i][j-1],dp[i-1][j]);
-            dp[i][j]=max(milse,not_milse);
+    bool isSubsequence(string s, string t) {
+        if(s.size()==0)return 1;
+        ll n=s.size(), m=t.size();
+        vector< vector<ll>>dp(n+1, vector<ll>(m+1,0));
+        for(ll i=1; i<=n; i++){
+            for(ll j=1; j<=m; j++){
+                if(s[i-1]==t[j-1])dp[i][j]=1+dp[i-1][j-1];
+                else dp[i][j]=max(dp[i-1][j], dp[i][j-1]);
+            }
         }
-       }
-        //cout<<dp[n][m]<<n1;
-        ll ans=dp[n][m];
-        cout<<ans;
-        return ans==s.size()-1;
+        //cout<<dp[n][m];
+        return dp[n][m]==n;
     }
 };
